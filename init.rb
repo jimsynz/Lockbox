f3783d135e564f06df96dde95794433a7f6d5bf8
James Harton <james@mashd.cc>
2009-08-24 10:30:02 +1200

    Added acts_as_lockbox functionality

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 require File.dirname(__FILE__) + '/lib/lockbox'
+ActiveRecord::Base.send(:include, MashdCc::Acts::Lockbox)
--- lib/lockbox.rb
+++ lib/lockbox.rb
@@ -34,7 +34,7 @@ class Lockbox
   def self.lock!
     @@public_key = nil
     @@private_key = nil
-    @locked == true
+    @@locked = true
   end
 
   def self.encrypt(val)
@@ -56,4 +56,59 @@ class Lockbox
 end
 
 class StillLockedException < RuntimeError
+end
+
+module MashdCc
+  module Acts 
+    module Lockbox 
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def acts_as_lockbox(*opts)
+          # Add the standard Lockbox API to the Model.
+          self.class_eval <<-RUBY
+            
+            def self.locked?
+              ::Lockbox.locked?
+            end
+
+            def self.unlocked?
+              ::Lockbox.unlocked?
+            end
+
+            def self.try? (pass_phrase)
+              ::Lockbox.try? pass_phrase
+            end
+
+            def self.lock!
+              ::Lockbox.lock!
+            end
+
+          RUBY
+          opts.each do |opt|
+            if opt.is_a? Hash and opt.key? :for
+              opt[:for].each do |field|
+                self.class_eval <<-RUBY
+
+                  def #{field}
+                    ::Lockbox.decrypt(read_attribute(:#{field}))
+                  end
+
+                  def #{field}=(val)
+                    write_attribute(:#{field}, ::Lockbox.encrypt(val))
+                  end
+
+                RUBY
+              end
+            end
+          end
+
+        end
+      end
+
+    end
+  end
 end
