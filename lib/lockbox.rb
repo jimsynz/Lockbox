043f2d07d4222b58dcbe9f9e8af821aff8374979
James Harton <james@mashd.cc>
2009-09-03 12:49:11 +1200

    We can always encrypt with the public key regardless of whether the private key has been unlocked.

--- lib/lockbox.rb
+++ lib/lockbox.rb
@@ -16,13 +16,12 @@ class Lockbox
   end
 
   def self.try? (pass_phrase)
-    config = YAML::load_file("#{RAILS_ROOT}/config/lockbox.yml")[RAILS_ENV]
-    if pass_phrase.nil? and config['pass_phrase'].nil?
+    @@config ||= YAML::load_file("#{RAILS_ROOT}/config/lockbox.yml")[RAILS_ENV]
+    if pass_phrase.nil? and @@config['pass_phrase'].nil?
       false
     else
       begin
-        @@public_key = OpenSSL::PKey::RSA.new(File.read(config['public_key_path']))
-        @@private_key = OpenSSL::PKey::RSA.new(File.read(config['private_key_path']), pass_phrase||config['pass_phrase'])
+        @@private_key = OpenSSL::PKey::RSA.new(File.read(@@config['private_key_path']), pass_phrase||@@config['pass_phrase'])
         @@locked = false
         true
       rescue OpenSSL::PKey::RSAError
@@ -32,15 +31,20 @@ class Lockbox
   end
 
   def self.lock!
-    @@public_key = nil
     @@private_key = nil
     @@locked = true
   end
 
   def self.encrypt(val)
-    if locked?
-      raise StillLockedException, "Passphrase not provided to lockbox."
-    elsif val.is_a? String
+    if val.is_a? String
+      @@config ||= YAML::load_file("#{RAILS_ROOT}/config/lockbox.yml")[RAILS_ENV]
+      if @@public_key.nil?
+        begin
+          @@public_key = OpenSSL::PKey::RSA.new(File.read(@@config['public_key_path']))
+        rescue
+          return false
+        end
+      end
       Base64.encode64(@@public_key.public_encrypt(val))
     end
   end
